8e36a557d6242aa3674bbe6966ed61a35f57cbbb
Travis Hoover <traviskhoover@gmail.com>
2020-03-30 23:27:03 -0500

    Test that a slack challenge works and returns the expected value.

--- app/tests/unit/test-handler.js
+++ app/tests/unit/test-handler.js
@@ -16,7 +16,6 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+        expect(response).to.have.all.keys('challenge');
     });
 });
