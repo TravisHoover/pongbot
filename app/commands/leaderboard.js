d24ac237e6b47bb9cfa04b102950313b773c45a8
Travis Hoover <traviskhoover@gmail.com>
2020-04-12 15:53:17 -0500

    Format the users, `mention` them in slack channel

--- app/commands/leaderboard.js
+++ app/commands/leaderboard.js
@@ -3,5 +3,13 @@ const usersTable = process.env.USERS_TABLE;
 
 module.exports.getLeaderboard = async () => {
   const users = await db.tableScan(usersTable);
-  return JSON.stringify(users);
+  let result = '';
+
+  users.forEach((user) => {
+    const formattedUser = `
+    <@${user.username}> ${user.wins}
+    `;
+    result += formattedUser;
+  })
+  return JSON.stringify(result);
 };
