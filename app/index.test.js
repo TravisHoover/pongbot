a8efd65ae6eab088e4614c6e4aa21ef0a4252c76
Travis Hoover <traviskhoover@gmail.com>
2020-05-03 12:44:28 -0500

    Finish covering most of index.js

--- app/index.test.js
+++ app/index.test.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const index = require('./index.js');
+const db = require('./utils/db');
 const slackChallenge = require('../events/slackChallenge.json');
 const registerMessage = require('../events/register.json');
 const challengeMessage = require('../events/challenge.json');
+const leaderboardMessage = require('../events/leaderboard.json');
+const wonMessage = require('../events/won.json');
+const unrecognizedMessage = require('../events/unrecognized.json');
 
 jest.mock('./utils/slack.js');
 
@@ -30,13 +34,47 @@ describe('Core tests', () => {
     expect(typeof response).toBe('object');
     expect(response).toHaveProperty('statusCode');
   })
-  test('register a user', async () => {
-    const response = await index.slackHandler(registerMessage);
-    expect(response).toHaveProperty('statusCode');
-    expect(response.body).toContain('registered');
+  describe('Register case', () => {
+    test('register a user', async () => {
+      const response = await index.slackHandler(registerMessage);
+      expect(response).toHaveProperty('statusCode');
+      expect(response.body).toContain('registered');
+    })
   })
-  test('make a challenge with an open game', async () => {
-    const challenge = await index.slackHandler(challengeMessage);
-    expect(challenge.body).toContain('A game is already in progress');
+  describe('Challenge case', () => {
+    test('make a challenge with an open game', async () => {
+      await db.putItem(
+        'Games',
+        {
+          ID: 'testID',
+          challenger: 'challenger',
+          opponent: 'opponent',
+          status: 'open',
+        },
+      );
+      const challenge = await index.slackHandler(challengeMessage);
+      expect(challenge.body).toContain('A game is already in progress');
+    })
+  })
+  describe('Won case', () => {
+    test('handle won command', async () => {
+      const results = await index.slackHandler(wonMessage);
+      console.log('results', results);
+      expect(results).toHaveProperty('statusCode');
+    })
+  })
+  describe('Leaderboard case', () => {
+    test('handle leaderboard command', async () => {
+      const results = await index.slackHandler(leaderboardMessage);
+      expect(results).toHaveProperty('statusCode');
+      expect(results.statusCode).toBe(200);
+    })
+  })
+  test('Unrecognized command', async () => {
+    const results = await index.slackHandler(unrecognizedMessage);
+    expect(results).toHaveProperty('statusCode');
+    expect(results.statusCode).toBe(200);
+    expect(results).toHaveProperty('body');
+    expect(results.body).toBe('\"Command not recognized\"');
   })
 })
