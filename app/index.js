d03cd11c121abad0d70f718ba581f0bb5cf28d7c
Travis Hoover <traviskhoover@gmail.com>
2020-05-06 23:02:18 -0500

    try catch not worth that 100%!

--- app/index.js
+++ app/index.js
@@ -37,65 +37,60 @@ const getGames = async () => {
  * @returns {*}
  */
 const slackHandler = async (event) => {
-    try {
-        let request = JSON.parse(event.body);
-        let response;
-        const user = request.event.user;
-        const message = request.event.text;
-        const conversationId = request.event.channel;
-        const splitMessage = message.split(" ");
+    let request = JSON.parse(event.body);
+    let response;
+    const user = request.event.user;
+    const message = request.event.text;
+    const conversationId = request.event.channel;
+    const splitMessage = message.split(" ");
 
-        /**
-         * To integrate with Slack, a 'challenge' must be made by Slack that sends a POST to the provided endpoint
-         * This challenge must reply with the message sent to authenticate the bot.
-         */
-        if (request.challenge) {
-            return {
-                'statusCode': 200,
-                'body': JSON.stringify({
-                    challenge: request.challenge,
-                })
-            }
+    /**
+     * To integrate with Slack, a 'challenge' must be made by Slack that sends a POST to the provided endpoint
+     * This challenge must reply with the message sent to authenticate the bot.
+     */
+    if (request.challenge) {
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                challenge: request.challenge,
+            })
         }
-
-        const openGame = await db.queryByIndex(gamesTable, 'status-index', 'status', 'open');
-
-        /**
-         * This will be used as a unified way to accept commands and arguments
-         * [0] Pongbot's user
-         * [1] Command
-         * [2] Opponent
-         * @type {Array|*|string[]}
-         */
-        switch (splitMessage[1]) {
-            case 'register':
-                response = await registerHandler.register(user);
-                break;
-            case 'challenge':
-                if (openGame.Count > 0) {
-                    await slack.postMessage(conversationId, 'A game is already in progress');
-                    return createResponse(400, 'A game is already in progress.')
-                }
-                response = await challengeHandler.challenge(user, splitMessage[2]);
-                break;
-            case 'won':
-                if (openGame.Count > 0) {
-                    response = await challengeHandler.won(openGame, user);
-                } else response = 'No games in progress.';
-                break;
-            case 'leaderboard':
-                response = await leaderboardHandler.getLeaderboard();
-                break;
-            default:
-                response = 'Command not recognized';
-        }
-
-        await slack.postMessage(conversationId, response);
-        return createResponse(200, response);
-    } catch (err) {
-        console.log(err);
-        return createResponse(400, err.message);
     }
+
+    const openGame = await db.queryByIndex(gamesTable, 'status-index', 'status', 'open');
+
+    /**
+     * This will be used as a unified way to accept commands and arguments
+     * [0] Pongbot's user
+     * [1] Command
+     * [2] Opponent
+     * @type {Array|*|string[]}
+     */
+    switch (splitMessage[1]) {
+        case 'register':
+            response = await registerHandler.register(user);
+            break;
+        case 'challenge':
+            if (openGame.Count > 0) {
+                await slack.postMessage(conversationId, 'A game is already in progress');
+                return createResponse(400, 'A game is already in progress.')
+            }
+            response = await challengeHandler.challenge(user, splitMessage[2]);
+            break;
+        case 'won':
+            if (openGame.Count > 0) {
+                response = await challengeHandler.won(openGame, user);
+            } else response = 'No games in progress.';
+            break;
+        case 'leaderboard':
+            response = await leaderboardHandler.getLeaderboard();
+            break;
+        default:
+            response = 'Command not recognized';
+    }
+
+    await slack.postMessage(conversationId, response);
+    return createResponse(200, response);
 };
 
 module.exports = {
